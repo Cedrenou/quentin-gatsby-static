83c24b19545e817bf6a1fa8db478ca369d349b44
Cedrenou <ced2411@gmail.com>
2020-02-11 15:31:06 +0100

    add 404 page and new souncloud iframe

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,33 +4,17 @@ import pic01 from '../images/pic01.jpg'
 
 class Main extends React.Component {
     render() {
-        let close = (
-            <div className="close" onClick={() => this.props.onCloseArticle()}></div>
-        )
+        let close = (<div className="close" onClick={() => this.props.onCloseArticle()}></div>)
 
         return (
-            <div
-                ref={this.props.setWrapperRef}
-                id="main"
-                style={this.props.timeout
-                ? {
-                    display: 'flex'
-                }
-                : {
-                    display: 'none'
-                }}>
+            <div ref={this.props.setWrapperRef}
+                 id="main"
+                 style={this.props.timeout ? {display: 'flex'} : {display: 'none'}}>
 
                 {/* ABOUT */}
-                <article
-                    id="about"
-                    className={`${this.props.article === 'about'
-                    ? 'active'
-                    : ''} ${this.props.articleTimeout
-                        ? 'timeout'
-                        : ''}`}
-                    style={{
-                    display: 'none'
-                }}>
+                <article id="about"
+                         className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`}
+                         style={{display: 'none'}}>
                     <h2 className="major">Qui suis-je ?</h2>
                     <span className="image main">
                         <img src={pic01} alt=""/>
@@ -56,100 +40,67 @@ class Main extends React.Component {
                 </article>
 
                 {/* WORK */}
-                <article
-                    id="work"
-                    className={`${this.props.article === 'work'
-                    ? 'active'
-                    : ''} ${this.props.articleTimeout
-                        ? 'timeout'
-                        : ''}`}
-                    style={{
-                    display: 'none'
-                }}>
+                <article id="work"
+                         className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`}
+                         style={{display: 'none'}}>
                     <h2 className="major">Mes sons</h2>
-                    <span className="image main">
-                        <iframe
-                            width="100%"
-                            height="300"
-                            scrolling="no"
-                            frameBorder="no"
-                            allow="autoplay"
-                            src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/659259596&color=%23050505&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
-
-                        <hr className="divider"></hr>
-
-                        <iframe
-                            width="100%"
-                            height="300"
-                            scrolling="no"
-                            frameBorder="no"
-                            allow="autoplay"
-                            src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/653291855&color=%23050505&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
-
-                    </span>
                     <p>
                         Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend
                         sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem
                         et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.
                     </p>
-                    <p>
-                        Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris
-                        aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In
-                        efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque
-                        lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et
-                        sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat
-                        tempus.
-                    </p>
+                    <span className="image main">
+                        <iframe width="100%"
+                                height="300"
+                                scrolling="no"
+                                frameBorder="no"
+                                allow="autoplay"
+                                src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/659259596&color=%23050505&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+
+                        <hr className="divider"></hr>
+
+                        <iframe width="100%"
+                                height="300"
+                                scrolling="no"
+                                frameBorder="no"
+                                allow="autoplay"
+                                src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/653291855&color=%23050505&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+
+                        <hr className="divider"></hr>
+
+                        <iframe width="100%"
+                                height="166"
+                                scrolling="no"
+                                frameBorder="no"
+                                allow="autoplay"
+                                src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/649854215&color=%23050505&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+
+                    </span>
                     {close}
                 </article>
 
                 {/* CONTACT */}
-                <article
-                    id="contact"
-                    className={`${this.props.article === 'contact'
-                    ? 'active'
-                    : ''} ${this.props.articleTimeout
-                        ? 'timeout'
-                        : ''}`}
-                    style={{
-                    display: 'none'
-                }}>
+                <article id="contact"
+                         className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`}
+                         style={{display: 'none'}}>
                     <h2 className="major">Contact</h2>
 
-                    <div>
-                        <ul className='icons'>
-                            <li>
-                                <a
-                                    href="https://www.youtube.com/channel/UCKSYmx-Rg2VpEc_KwKJVjdA"
-                                    target='_blank'
-                                    className="icon fa-envelope">
-                                    <span className="label">Mon Email: lorem@ipsum.com</span>
-                                </a>
-                            </li>
-                        </ul>
-
-                        <p>Mon Tel: 06.22.23.24.25</p>
-                    </div>
-
-                    <form
-                        method="POST"
-                        name="contact form"
-                        action="/success"
-                        data-netlify="true">
+                    <form method="POST"
+                          name="contact form"
+                          action="/success"
+                          data-netlify="true">
 
                         <input type="hidden" name="form-name" value="contact form"/>
 
-                        <p style={{display: 'none'}}>
-                            <label>Don’t fill this out if you're human:
-                                <input name="bot-field"/></label>
-                        </p>
+                        <p style={{display: 'none'}}><label>Don’t fill this out if you're human:<input
+                            name="bot-field"/></label></p>
 
                         <div className="field half first">
-                            <label htmlFor="name">Name</label>
+                            <label htmlFor="name">Nom</label>
                             <input type="text" name="contact-name" id="name"/>
                         </div>
                         <div className="field half">
-                            <label htmlFor="email">Email</label>
+                            <label htmlFor="email">E-mail</label>
                             <input type="text" name="contact-email" id="email"/>
                         </div>
                         <div className="field">
@@ -157,13 +108,7 @@ class Main extends React.Component {
                             <textarea name="contact-message" id="message" rows="4"></textarea>
                         </div>
                         <ul className="actions">
-                            <li>
-                                <button type="submit">SUBMIT</button>
-                                {/*<input type="submit" value="Send Message" className="special"/>*/}
-                            </li>
-                            <li>
-                                <input type="reset" value="Reset"/>
-                            </li>
+                            <li><input type="submit" value="Envoyer" className="special"/></li>
                         </ul>
 
                         <div data-netlify-recaptcha="true"></div>
@@ -172,18 +117,16 @@ class Main extends React.Component {
 
                     <ul className="icons">
                         <li>
-                            <a
-                                href="https://www.youtube.com/channel/UCKSYmx-Rg2VpEc_KwKJVjdA"
-                                target='_blank'
-                                className="icon fa-youtube-play">
+                            <a href="https://www.youtube.com/channel/UCKSYmx-Rg2VpEc_KwKJVjdA"
+                               target='_blank'
+                               className="icon fa-youtube-play">
                                 <span className="label">Twitter</span>
                             </a>
                         </li>
                         <li>
-                            <a
-                                href="https://www.instagram.com/beatbox.vd/"
-                                target='_blank'
-                                className="icon fa-instagram">
+                            <a href="https://www.instagram.com/beatbox.vd/"
+                               target='_blank'
+                               className="icon fa-instagram">
                                 <span className="label">Instagram</span>
                             </a>
                         </li>
--- src/pages/404.js
+++ src/pages/404.js
@@ -2,10 +2,11 @@ import React from 'react'
 import Layout from '../components/layout'
 
 const NotFoundPage = () => (
-  <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+    <Layout>
+        <h1>404</h1>
+        <p>La page recherchée est introuvable</p>
+        <a href="/">Retour à la page principale</a>
+    </Layout>
 )
 
 export default NotFoundPage
